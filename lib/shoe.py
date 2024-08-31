2179a10edb866a9c94a5d9095db231c41c4179e2
R-kjim <rkayjim@gmail.com>
2024-08-31 12:03:37 +0300

    oop practice

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand="Adidas",size=9):
+        self.brand=brand
+        self.size=size
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self,size):
+        if(type(size)==int):
+            self._size=size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition="New"
+
+   
+        
+shoe=Shoe()
+print(shoe.brand)
+print(shoe.size)
+shoe.cobble()
+print (shoe.condition)
